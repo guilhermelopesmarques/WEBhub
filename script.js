ae205be8a1d04a480e77167ea466c31015232cb2
guilherme <112526329+guilhermelopesmarques@users.noreply.github.com>
2026-04-14 15:09:38 -0300

    animação

---
+++ script.js
@@ -0,0 +1,18 @@
+// objetos console
+// metodos
+
+// eventos
+
+window.addEventListener("load", () => {
+    const videohero = document.querySelector(".hero video");
+
+    videohero.src = "img/video-hero.mp4";
+    videohero.autoplay = true;
+    videohero.loop = true;
+    videohero.muted = true;
+
+});
+
+
+
+
