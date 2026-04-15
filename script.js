21c65c54dd93a66bcdc778ea4fc8d1b1f53abaa5
guilherme <112526329+guilhermelopesmarques@users.noreply.github.com>
2026-04-15 15:09:13 -0300

    transicao

--- script.js
+++ script.js
@@ -4,14 +4,23 @@
 // eventos
 
 window.addEventListener("load", () => {
-    const videohero = document.querySelector(".hero video");
+    const videohero = document.querySelector(".hero video");   
+    const videoFooter = document.querySelector("footer video"); 
+    
+    console.log(videoFooter)
 
     videohero.src = "img/video-hero.mp4";
     videohero.autoplay = true;
     videohero.loop = true;
     videohero.muted = true;
 
+    videoFooter.src = "img/video-footer.mp4";
+    videoFooter.autoplay = true;
+    videoFooter.loop = true;
+    videoFooter.muted = true;
+
 });
+
 
 
 
